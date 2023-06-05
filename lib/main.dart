072d024dc2a056ecf52317a64b0d7094211782b8
Daksha Deep <dakshadeep1234@gmail.com>
2023-06-05 15:17:25 +0530

    Adding an image to the home page and button

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,44 @@ void main() {
       appBar: AppBar(
         title: const Text('Home'),
       ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+        ),
+      ),
     ),
   ));
+
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    ),
+  ));
+
+  // return runApp(MaterialApp(
+  //   home: Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Home'),
+  //     ),
+  //     body: Center(
+  //       child: ElevatedButton(
+  //         onPressed: () {},
+  //         child: Text('Click Me'),
+  //       ),
+  //     ),
+  //   ),
+  // ));
 }
