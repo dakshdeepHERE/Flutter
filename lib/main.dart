c7b7d116664a2dfbdf8cef821b15da30c23315bd
Daksha Deep <dakshadeep1234@gmail.com>
2023-06-15 15:03:47 +0530

    Created a Column and children inside it

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,57 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 252, 70, 57),
+        appBar: AppBar(
+          title: const Text('Home'),
         ),
-        child: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+        body: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 252, 70, 57),
+              ),
+              child: const Center(
+                child: Text(
+                  'Hello World!',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
-    ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 252, 70, 57),
+              ),
+              child: const Center(
+                child: Text(
+                  'Im new to coding',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 252, 70, 57),
+              ),
+              child: const Center(
+                child: Text(
+                  'Beginner to pro',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
   ));
 }
